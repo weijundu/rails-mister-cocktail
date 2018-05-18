dbe038f0944633b1efdadca47e9ae78af57fab08
Weijun Du <weijundu883@163.com>
2018-05-19 03:27:31 +0800

    done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -12,7 +12,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to root_path
     else
       render :new
     end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -10,7 +10,7 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to root_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
@@ -19,7 +19,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to root_path(@cocktail)
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
